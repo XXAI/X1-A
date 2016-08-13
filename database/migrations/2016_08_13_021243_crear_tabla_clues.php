3ba4d77704ad34f00bc7c7128160c04d1b9e5d69
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2016-08-12 21:18:41 -0500

    se Agrego migration para la tabla clues

---
+++ database/migrations/2016_08_13_021243_crear_tabla_clues.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaClues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('clues',12);
+            $table->string('nombre',255);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
