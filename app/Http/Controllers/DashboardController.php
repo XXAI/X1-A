e0e8de72383f104223e1e9cabbcc881bccb41017
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2016-10-21 10:44:36 -0500

    actualizar campos en actas y se agrego funcionalidad del dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Usuario as Usuario;
+use App\Models\Acta as Acta;
 
 use Input, Response,  Validator;
 use Illuminate\Http\Response as HttpResponse;
@@ -24,6 +25,20 @@ class DashboardController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function index(){
+	public function index(Request $request){
+		$user_email = $request->header('X-Usuario');
+
+		$actas_sin_validar = Acta::where('estatus',2)->count();
+
+		$actas = Acta::with('UnidadMedica')
+                        ->take(10)
+                        ->where('estatus',2)
+                        ->orderBy('fecha_importacion','asc')
+                        ->get();
+        $datos = [
+        	'actas' => $actas,
+        	'actas_sin_validar'=> $actas_sin_validar
+        ];
+        return Response::json(['data'=>$datos],200);
 	}
 }
